923eb34108802d962263dd36c35f5f609fcaad5c
Szymon Richert <szymon.rychu@gmail.com>
2019-12-22 10:21:41 +0100

    Split containers

--- start_freenect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/ros/${ROS_DISTRO}/setup.bash
-roslaunch freenect_launch freenect.launch depth_registration:=true
--- start_rtabmap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/ros/${ROS_DISTRO}/setup.bash
-sleep 10
-roslaunch rtabmap_ros visual_odometry.launch rtabmap_args:="--delete_db_on_start" rviz:=true rtabmapviz:=false
